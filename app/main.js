73e66de02029ca1123af05f7ca1e28e6570106fd
obalbous <oba@squid>
2015-01-21 11:31:25 +0100

    DASH-88 If no preview done, the exported analysis is wrong

--- app/main.js
+++ app/main.js
@@ -63,7 +63,7 @@ var totalAnalysis = new api.model.AnalysisJob();
 // note model objects references contain oids
 var mainModel = new Backbone.Model({
     "timeDimension": null,
-    "currentAnalysis" : null,
+    "currentAnalysis" : exportAnalysis,
     "totalAnalysis" : totalAnalysis,
     "chosenDimensions" : [],
     "selectedDimension" :  null,
