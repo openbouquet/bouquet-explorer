a102cacea2d0ef95e5ef2a99b4c707caf706d26d
Thomas Scott <thomas.scott@squidsolutions.com>
2015-10-22 17:40:07 +0200

    add timeseries option

--- app/app.js
+++ app/app.js
@@ -161,6 +161,7 @@ var tableView = new squid_api.view.DataTableView ({
 var timeView = new squid_api.view.TimeSeriesView ({
     el : '#timeView',
     model : timeAnalysis,
+    multiSeries : true,
     colorPalette : squid_api.view.metadata,
     interpolationRange : 'months',
     staleMessage : "Click preview to update"
