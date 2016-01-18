fff2ec465a888567733eb42dbeb4192ac324b561
olivier <obalbous@squidsolutions.com>
2016-01-18 13:59:17 +0100

    T757 do not set analysis status to RUNNING

--- app/app.js
+++ app/app.js
@@ -452,8 +452,8 @@ config.on("change:startIndex", function(config) {
     if ((startIndex || startIndex === 0) && (startIndex !== config.get("startIndex"))) {
         // update if pagination changed
         a.setParameter("startIndex", config.get("startIndex"));
-        a.set("status", "RUNNING");
         if (a.get("id") && (a.get("id").analysisJobId)) {
+            a.set("status", "RUNNING");
             squid_api.controller.analysisjob.getAnalysisJobResults(null, a);
         }
     }
