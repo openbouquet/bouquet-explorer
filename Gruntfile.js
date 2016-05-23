b1bc8e9dc353d1f388339c5f6ab532aa1cc2d215
Louis Rabiet <louis.rabiet@squidsolutions.com>
2016-05-23 18:01:06 +0200

    Adding Bouquet Mode for ACE

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,14 @@ module.exports = function (grunt) {
             },
             copy: {
                 main: {
-                    files: [{
+                    files: [
+			{
+                                expand: true,
+				flatten: true,
+                                src: ['bower_components/squid_api_admin_widgets/mode/mode-bouquet.js'],
+                                dest: 'bower_components/ace-builds/src/'
+                        },
+			{
                         expand: true,
                         src: [
                             "app/app.js",
@@ -76,8 +83,9 @@ module.exports = function (grunt) {
                             return dest
                                 + src.replace(/\.template.html$/,
                                     ".html");
-                        }
-                    }]
+                        	}
+                    	}
+		]
                 }
             },
             wiredep: {
